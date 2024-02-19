879f5fa6d568a9d9f876d79c67280b4b3d33df50
toxpy <natoxpy@gmail.com>
2024-02-19 13:34:32 -0500

    fix: Add api soundcloud import endpoint
    - Added api method to import soundcloud music. note: This was accidentally left out from previous commit.

---
+++ src/stores/api/import/soundcloud.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { API_LOCATION, type Music } from "..";
+
+export type SoundcloudImport = {
+  title: string;
+  image_id: string;
+  soundcloud_url: string;
+  artists_id: string;
+  featured_artists_id: string[];
+  album_id: string;
+};
+
+export async function soundcloudImport(data: SoundcloudImport): Promise<Music> {
+  const res = await axios.post(`${API_LOCATION}/soundcloud/import/`, data);
+  return res.data as Music;
+}
