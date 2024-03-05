cefe861579cd69d7e1f0503279ae2d87d73083a9
toxpy <natoxpy@gmail.com>
2024-03-05 12:20:18 -0500

    refactor: Move player controls into module

---
+++ src/modules/audioPlayer/components/controls/ActionButtons.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import PlayButton from '@/components/icons/PlayBtn.vue'
+import PauseButton from '@/components/icons/PauseBtn.vue'
+import PreviousButton from '@/components/icons/PreviousBtn.vue'
+import NextButton from '@/components/icons/NextBtn.vue'
+import RepeatIcon from '@/components/icons/RepeatIcon.vue'
+import RepeatAllIcon from '@/components/icons/RepeatAllIcon.vue'
+import RepeatOneIcon from '@/components/icons/RepeatOneIcon.vue'
+import { onMounted } from 'vue'
+// import { audioStore } from '../../stores/audio'
+
+// let audioManager = audioStore()
+
+function Play() {
+    // audioManager.play()
+}
+
+function Pause() {
+    // audioManager.pause()
+}
+</script>
+<template>
+    <div class="w-full ml-4 max-w-44 h-full flex items-center justify-center gap-1">
+        <button class="p-2">
+            <PreviousButton :size="20" />
+        </button>
+        <button @click="// audioManager.paused ? Play() : Pause()" class="p-2">
+            <PauseButton v-if="// !audioManager.paused" :size="20" />
+            <PlayButton v-else :size="20" />
+        </button>
+        <button class="p-2">
+            <NextButton :size="20" />
+        </button>
+        <button class="p-2">
+            <RepeatIcon :size="22" />
+        </button>
+    </div>
+</template>
---
+++ src/modules/audioPlayer/components/controls/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TimeSeeker from './TimeSeeker.vue'
+import ActionButtons from './ActionButtons.vue'
+import VolumeButtons from './VolumeButtons.vue'
+</script>
+
+<template>
+  <div class="border-t border-baccent w-full gap-4 h-full flex justify-center items-center">
+    <ActionButtons />
+    <div class="w-1 h-1 rounded-full bg-gray-400" />
+    <TimeSeeker />
+    <div class="w-1 h-1 rounded-full bg-gray-400" />
+    <VolumeButtons />
+  </div>
+</template>
